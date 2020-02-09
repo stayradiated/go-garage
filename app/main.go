fc01fb6ba0d6190f822939d2ba380876b9892723
George Czabania <1140167+stayradiated@users.noreply.github.com>
2020-02-09 15:23:36 +1300

    refactor: switch to gpiod

--- app/main.go
+++ app/main.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
-	"github.com/warthog618/gpio"
+	"github.com/warthog618/gpiod"
+	"github.com/warthog618/gpiod/device/rpi"
 )
 
 const (
@@ -18,8 +22,6 @@ const (
 var (
 	addr      = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
 	doorState = DOOR_SHUT
-	rate      = time.Second / 2
-	throttle  = time.Tick(rate)
 )
 
 func readDoorState(w http.ResponseWriter, req *http.Request) {
@@ -31,40 +33,54 @@ func readDoorState(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, message)
 }
 
-func handler(pin *gpio.Pin) {
-	<-throttle //rate limit
-
-	level := pin.Read()
-	if level == gpio.Low {
-		log.Println("low")
-		doorState = DOOR_SHUT
-	} else {
-		log.Println("high")
-		doorState = DOOR_OPEN
-	}
-}
-
 func main() {
 	flag.Parse()
 
-	err := gpio.Open()
+	c, err := gpiod.NewChip("gpiochip0")
 	if err != nil {
 		panic(err)
 	}
+	defer c.Close()
 
-	outputPin := gpio.NewPin(2)
-	outputPin.Output()
+	led := 0
+
+	l, err := c.RequestLine(rpi.GPIO2, gpiod.AsOutput(led))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		l.Reconfigure(gpiod.AsInput)
+		l.Close()
+	}()
+
+	// capture exit signals to ensure pin is reverted to input on exit.
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(quit)
 
 	go func() {
 		for {
-			outputPin.Toggle()
-			time.Sleep(1 * time.Second)
+			select {
+			case <-time.After(2 * time.Second):
+				led ^= 1
+				l.SetValue(led)
+			case <-quit:
+				return
+			}
 		}
 	}()
 
-	inputPin := gpio.NewPin(6)
-	inputPin.Input()
-	inputPin.Watch(gpio.EdgeBoth, handler)
+	l2, err := c.RequestLine(rpi.GPIO6, gpiod.WithBothEdges(func(evt gpiod.LineEvent) {
+		edge := "rising"
+		if evt.Type == gpiod.LineEventFallingEdge {
+			edge = "falling"
+		}
+		log.Printf("event:%3d %-7s\n", evt.Offset, edge)
+	}))
+	if err != nil {
+		panic(err)
+	}
+	defer l2.Close()
 
 	http.HandleFunc("/state", readDoorState)
 	http.ListenAndServe(*addr, nil)
